3bf7bd1dbf8b1e9e0bd7d386a1438b1bb8ef729f
Marc Roige Benaiges <marc.roige@estudiants.urv.cat>
2022-03-16 14:01:52 +0100

    XMLRPC falta groupby

--- InsultServerXMLRPC/Client.py
+++ InsultServerXMLRPC/Client.py
@@ -1,7 +1,37 @@
 import xmlrpc.client
 
-#s = xmlrpc.client.ServerProxy('http://localhost:8000')
-s = xmlrpc.client.ServerProxy('http://10.21.4.0:8080')
-print(s.system.listMethods())
-#print(s.getInsults())
+import pandas as pd
+
+if __name__ == "__main__":
+    # s = xmlrpc.client.ServerProxy('http://localhost:8000')
+    s = xmlrpc.client.ServerProxy('http://localhost:8080')
+    print(s.system.listMethods())
+    print(s.getinsult())
+
+    print("---------LLEGIR FITXER CSV--------------")
+    #print(s.readcsv())
+
+    print("---------APPLY--------------")
+    #print(s.applydf("lambda x: x**2"))
+
+    print("---------COLUMNS--------------")
+    #print(s.columns())
+
+    print("---------GROUPBY--------------")
+    print(s.groupby("A"))
+
+    print("---------HEAD--------------")
+    print(s.head())
+
+    print("---------ISIN--------------")
+    print(s.isin())
+
+    print("---------ITEMS--------------")
+    #print(s.items())
+
+    print("---------MAX--------------")
+    print(s.max())
+
+    print("---------MIN--------------")
+    print(s.min())
 
