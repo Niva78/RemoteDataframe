0dbfa649ce9d0ad300c40be070635d1f5eb6732d
Nicolas Vanegas Herrera <nicolas.vanegas@estudiants.urv.cat>
2022-03-18 13:28:43 +0100

    Nicolas: XMLrpc work distribution

--- XMLRPC/Client.py
+++ XMLRPC/Client.py
@@ -2,6 +2,15 @@ import xmlrpc.client
 
 import pandas as pd
 
+#PENDIENTE
+
+#Crear lista de workers
+
+#Pedir lista de workers al master
+
+#Cliente ha de propagar la ejecucion de las funciones a los diferentes worker 
+#Hacer bucle que envie la funcion a todos los worker disponible
+
 if __name__ == "__main__":
     # s = xmlrpc.client.ServerProxy('http://localhost:8000')
     s = xmlrpc.client.ServerProxy('http://localhost:8080')
---
+++ XMLRPC/Master.py
@@ -0,0 +1,7 @@
+#Lista de workers -> Contiene la URL de todos los worker
+
+#Metodo addWorkers(URL) -> Agrega un worker que se conecte
+
+#Metodo removeWorkers(URL) -> Quita a un worker pasado por parametro
+
+#Metodo getWorkers -> Pasa una lista de workers
