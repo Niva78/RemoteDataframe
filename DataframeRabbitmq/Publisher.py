de8507426a49866bb532e2eb180fad5b0fa991e5
Nicolas Vanegas Herrera <nicolas.vanegas@estudiants.urv.cat>
2022-05-18 12:16:39 +0200

    Modifying commit

--- DataframeRabbitmq/Publisher.py
+++ DataframeRabbitmq/Publisher.py
@@ -15,24 +15,22 @@ channel = connection.channel()
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 #Creating message
-message = ("min(\"Payment\")," + RESPONSE_QUEUE_NAME)
+message1 = ("min(\"Payment\")," + RESPONSE_QUEUE_NAME)
+message2 = ("max(\"Payment\")," + RESPONSE_QUEUE_NAME)
 
 #Publishing a message
-channel.basic_publish(exchange='logs', routing_key='', body=message)
+channel.basic_publish(exchange='logs', routing_key='', body=message1)
+channel.basic_publish(exchange='logs', routing_key='', body=message2)
 
 
 
 #Creating answer buffer
 channel.queue_declare(queue=RESPONSE_QUEUE_NAME, durable=True)
 
-min = 0
-max = 0
 
 def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    global max
-    result = pickle.loads(body)
-    max = max(max, int(result))
+    print(pickle.loads(body))
 
 
 channel.basic_consume(queue=RESPONSE_QUEUE_NAME, on_message_callback=callback)
